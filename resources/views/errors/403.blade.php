45704532cd3b59bf3a74b985b3a2a70c9b0545a5
NadetDev <nadetdev@gmail.com>
2024-03-20 17:20:58 +0100

    PHP Larevel Note App Tuto - End

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+   <div class="note-container py-12">
+    <div class="bg-red-500 py-5">
+        <h1>You are not the owner of this note!</h1>
+    </div>
+    
+   </div>
+</x-app-layout>
