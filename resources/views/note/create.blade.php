459cc3b5e8a1d6ff8a4e3aa2dd4fe26724c8e311
NadetDev <nadetdev@gmail.com>
2024-03-21 21:51:05 +0100

    modif. wording

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="note-container single-note">
-        <h1>Create new note</h1>
+        <h1>Create a new note</h1>
         <form action="{{ route('note.store') }}" method="POST" class="note">
             @csrf
             <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
